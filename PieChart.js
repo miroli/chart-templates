52838cb01908e8e40be0f91f070f8178bf95f9c5
svtpejl <robin_heter_jag@yahoo.se>
2014-02-25 16:05:47 +0100

    re-indented js

--- PieChart.js
+++ PieChart.js
@@ -2,69 +2,69 @@ var PejlCharts = PejlCharts || {};
 
 // The charts
 PejlCharts.PieChart = function() {
-	var div = d3.select("body").append("div")
-    	.attr("class", "tooltip")
-    	.style("opacity", 0);
+  var div = d3.select("body").append("div")
+      .attr("class", "tooltip")
+      .style("opacity", 0);
 
-	var margin = {top: 150, right: 20, bottom: 30, left: 150};
+  var margin = {top: 150, right: 20, bottom: 30, left: 150};
 
-	var conf = {
-		data: [],
-		color: d3.scale.category10(),
-		colorBy: '',
-		outerRadius: 50,
-		innerRadius: 40,
-		width: null,
-		height: null
-	};
+  var conf = {
+    data: [],
+    color: d3.scale.category10(),
+    colorBy: '',
+    outerRadius: 50,
+    innerRadius: 40,
+    width: null,
+    height: null
+  };
 
-	function addChartMethod(property) {
-		return function(value) {
-			if (!arguments.length) return conf[property];
-			conf[property] = value;
-			return chart;
-		}
-	}
+  function addChartMethod(property) {
+    return function(value) {
+      if (!arguments.length) return conf[property];
+      conf[property] = value;
+      return chart;
+    }
+  }
 
-	for (var prop in conf) {
-		if (conf.hasOwnProperty(prop)) {
-			chart[prop] = addChartMethod(prop);
-		}
-	}
+  for (var prop in conf) {
+    if (conf.hasOwnProperty(prop)) {
+      chart[prop] = addChartMethod(prop);
+    }
+  }
 
-	function chart(svg) {
-			conf.width = svg.style('width').replace('px','') - (margin.left + margin.right);
-			conf.height = svg.style('height').replace('px','') - (margin.top + margin.bottom);
+  function chart(svg) {
+      conf.width = svg.style('width').replace('px','') - (margin.left + margin.right);
+      conf.height = svg.style('height').replace('px','') - (margin.top + margin.bottom);
 
-		var g = svg.append('g')
-			.data([conf.data])
-			.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-			.attr('class', 'pejl-piechart');
+    var g = svg.append('g')
+      .data([conf.data])
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+      .attr('class', 'pejl-piechart');
 
-		var arc = d3.svg.arc()
-			.innerRadius(conf.innerRadius)
-			.outerRadius(conf.outerRadius);
+    var arc = d3.svg.arc()
+      .innerRadius(conf.innerRadius)
+      .outerRadius(conf.outerRadius);
 
-		var pie = d3.layout.pie()
-			.value(function(d) { return d.age });
+    var pie = d3.layout.pie()
+      .value(function(d) { return d.age });
 
-		var arcs = g.selectAll('g.slice')
-			.data(pie)
-			.enter()
-				.append('g')
-				.attr('class','slice');
+    var arcs = g.selectAll('g.slice')
+      .data(pie)
+      .enter()
+        .append('g')
+        .attr('class','slice');
 
-		arcs.append('path')
-			.attr('fill', function(d) { return conf.color(d.data[conf.colorBy]); })
-			.attr('d', arc);
+    arcs.append('path')
+      .attr('fill', function(d) { return conf.color(d.data[conf.colorBy]); })
+      .attr('d', arc);
 
-		d3.select(window).on('resize', resize);
+    d3.select(window).on('resize', resize);
 
-		function resize() {
-			conf.width = svg.style('width').replace('px','') - (margin.left + margin.right);
-			conf.height = svg.style('height').replace('px','') - (margin.top + margin.bottom);
-		}
-	}
+    function resize() {
+      conf.width = svg.style('width').replace('px','') - (margin.left + margin.right);
+      conf.height = svg.style('height').replace('px','') - (margin.top + margin.bottom);
+    }
+  }
 
-	return chart;
+  return chart;
 }
--- app.js
+++ app.js
@@ -4,27 +4,27 @@ d3.scale.pejlSignals = d3.scale.ordinal().range(['#079100','#C3CE0F','#F49D06','
 d3.scale.kuler = d3.scale.ordinal().range(['#B6D0F2','#307367','#025940','#F2EAE4','#F25E3D']);
 
 var svg = d3.select('#chart')
-	.append('svg')
-	.attr('width', '100%')
-	.attr('height', '100%');
+  .append('svg')
+  .attr('width', '100%')
+  .attr('height', '100%');
 
 // Instantiate a new Pejlchart
 d3.csv('data.csv', function(data) {
-	// var barChart = PejlCharts.BarChart()
-	// 	.data(data)
-	// 	.distribution('name')
-	// 	.value('age')
-	// 	.color(d3.scale.category20())
-	// 	.colorBy('name');
+  // var barChart = PejlCharts.BarChart()
+  //  .data(data)
+  //  .distribution('name')
+  //  .value('age')
+  //  .color(d3.scale.category20())
+  //  .colorBy('name');
 
-	// barChart(svg);
+  // barChart(svg);
 
-	var pieChart = PejlCharts.PieChart()
-		.data(data)
-		.outerRadius(150)
-		.innerRadius(100)
-		.color(d3.scale.kuler)
-		.colorBy('name');
+  var pieChart = PejlCharts.PieChart()
+    .data(data)
+    .outerRadius(150)
+    .innerRadius(100)
+    .color(d3.scale.kuler)
+    .colorBy('name');
 
-	pieChart(svg);
+  pieChart(svg);
 });
